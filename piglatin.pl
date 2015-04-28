c9d3d0cb9f376d7bf848daff3f8445b754251880
Caio Homar <caioah@users.noreply.github.com>
2015-04-28 12:45:50 -0300

    Update piglatin.pl

--- piglatin.pl
+++ piglatin.pl
@@ -1,14 +1,17 @@
 vowels("AEIOUaeiou").
-except("q", "u").
+except1("Qq").
+except2("Uu").
+
+except([C,V]) :- except1(L1),except2(L2),member(C,L1),member(V,L2).
 
 vowel(X) :- vowels(Ls),member(X,Ls).
 
 firstv([H|_]) :- vowel(H).
 
-firstc1([C,E|_]) :- except([C],[E]),!,fail.
+firstc1([C,E|_]) :- except([C,E]),!,fail.
 firstc1([C,V|_]) :- \+vowel(C),vowel(V).
 
-firstc2([C,E|_]) :- except([C],[E]).
+firstc2([C,E|_]) :- except([C,E]).
 firstc2([A,B|_]) :- \+vowel(A),\+vowel(B).
 
 inv1([H|T]) --> T,[H].
