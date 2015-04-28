34bf648248a4a0d3cc027bd8d2bb0cedf4888217
Caio Homar <caioah@users.noreply.github.com>
2015-04-28 11:45:42 -0300

    Update piglatin.pl

--- piglatin.pl
+++ piglatin.pl
@@ -1,29 +1,34 @@
-letters([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
-vowels([a,e,i,o,u,y]).
+letters("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").
+vowels("AEIOUYaeiouy").
+except("q", "u").
 
 letter(X) :- letters(Ls),member(X,Ls).
 vowel(X) :- vowels(Ls),member(X,Ls).
 
 firstv([H|_]) :- vowel(H).
+
+firstc1([C,E|_]) :- except([C],[E]),!,fail.
 firstc1([C,V|_]) :- \+vowel(C),vowel(V).
+
+firstc2([C,E|_]) :- except([C],[E]).
 firstc2([A,B|_]) :- \+vowel(A),\+vowel(B).
 
 inv1([H|T]) --> T,[H].
 inv2([A,B|T]) --> T,[A,B].
 
-pigw(X) --> {maplist(letter,X),firstv(X)},X,[w,a,y].
-pigw(X) --> {maplist(letter,X),firstc1(X)},inv1(X),[a,y].
-pigw(X) --> {maplist(letter,X),firstc2(X)},inv2(X),[a,y].
+pigw(X) --> {firstv(X)},X,"way".
+pigw(X) --> {firstc1(X)},inv1(X),"ay".
+pigw(X) --> {firstc2(X)},inv2(X),"ay".
 
 pigp([]) --> [].
-pigp([H|T]) --> {pigw(H,X,[])},[X],pigp(T).
+pigp([H|T]) --> {pigw(H,X,[])},[X],pigp(T),!.
 
 split([]) --> [].
-split(X) --> {\+member(' ',X)},[X].
-split(X) --> {append(A,[' '|B],X),\+member(' ',A)},[A],split(B).
+split(X) --> {\+member(32,X)},[X].
+split(X) --> {append(A,[32|B],X),\+member(32,A)},[A],split(B).
 
 join([]) --> {fail}.
 join([X]) --> X.
-join([H|T]) --> H,[' '],join(T).
+join([H|T]) --> H,[32],join(T).
 
-piglatin(X,Y) :- atom_chars(X,C),split(C,Ca,[]),pigp(Ca,Pa,[]),join(Pa,P,[]),atom_chars(Y,P).
+piglatin(X,Y) :- split(X,Ca,[]),pigp(Ca,Pa,[]),join(Pa,P,[]),name(Y,P).
